a01613440131dde1178d4b2fbf60d9ac08d3850f
concreted <arichuang@gmail.com>
2014-10-20 15:01:55 -0700

    Rename piece files, add karma tests

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,9 @@
-var chai = require('chai');
-var assert = chai.assert;
-
-it('should test something', function() {
-  assert.equal(1, 1);
+it('should clear rows from the grid when full', function() {
+  var field = new Field(1, 1);
+  assert.deepEqual(field.grid, [[0]]);
+  field.grid[0][0] = 1;
+  assert.deepEqual(field.grid, [[1]]);
+  field.clearRows();
+  assert.deepEqual(field.grid, [[0]]);
+  assert.equal(field.grid.length, 1);
 })
